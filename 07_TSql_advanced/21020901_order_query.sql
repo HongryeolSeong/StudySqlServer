851bbdf67b1e0db98d9a646b7f761d6310d6bd5f
ghd0276 <ghd0276@naver.com>
2021-02-09 12:45:02 +0900

    2/9 점심전 커밋

---
+++ 07_TSql_advanced/21020901_order_query.sql
@@ -0,0 +1,20 @@
+﻿use sqlDB;
+go
+
+-- 순위 함수
+select ROW_NUMBER() over(order by height desc, userName asc) as '키큰순위',
+       userName, addr, height
+  from userTbl;
+
+select RANK() over(order by height desc) as '키큰순위',
+       userName, addr, height
+  from userTbl;
+
+select DENSE_RANK() over(order by height desc) as '키큰순위',
+       userName, addr, height
+  from userTbl;
+
+-- 많이쓰는 구문 over(partition by A order by B desc)
+select RANK() over(partition by addr order by height desc) as '키큰순위',
+       userName, addr, height
+  from userTbl;
---
+++ 07_TSql_advanced/21020902_pivot_query.sql
@@ -0,0 +1,29 @@
+﻿use sqlDB;
+go
+
+/*
+create table pivotTBL
+(
+	userName nvarchar(10),
+	season nvarchar(3),
+	amount int
+);
+
+insert into pivotTBL values ('김범수', '겨울', 10),
+							('윤종신', '여름', 15),
+							('김범수', '가을', 25),
+							('김범수', '봄', 3),
+							('김범수', '봄', 37),
+							('윤종신', '겨울', 40),
+							('김범수', '여름', 14),
+							('김범수', '겨울', 22),
+							('윤종신', '여름', 64);
+go
+*/
+
+select * from pivotTBL;
+
+select * from pivotTBL
+ pivot (sum(amount)
+        for season
+		in ([봄], [여름], [가을], [겨울])) as resultPivot;
