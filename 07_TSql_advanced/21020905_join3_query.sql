07986c0ac82bc08f1c42513526b3ea3b9855d5db
ghd0276 <ghd0276@naver.com>
2021-02-09 16:27:15 +0900

    파일명 수정

---
+++ 07_TSql_advanced/21020905_join3_query.sql
@@ -0,0 +1,17 @@
+﻿select *
+  from StdTbl as s
+  left outer join ClubRegTbl as r
+    on s.id = r.std_id
+  full outer join ClubTbl as c
+    on r.club_id = c.id;
+
+select *
+  from StdTbl as s
+  left outer join ClubRegTbl as r
+    on s.id = r.std_id;
+
+
+select *
+  from ClubRegTbl as r
+ right outer join ClubTbl as c
+    on r.club_id = c.id;
