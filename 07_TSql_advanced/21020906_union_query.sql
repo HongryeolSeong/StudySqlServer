25baa85b3040c9bf790b3c6c71363b5eb173b303
ghd0276 <ghd0276@naver.com>
2021-02-09 17:01:35 +0900

    2/9 union 까지

---
+++ 07_TSql_advanced/21020906_union_query.sql
@@ -0,0 +1,14 @@
+﻿-- union
+select clubName as bName, buildingNum as bLocation from ClubTbl
+union
+select stdName, region from StdTbl;
+
+-- union all
+select stdName, region from stdTbl
+union all
+select stdName, region from stdTbl;
+
+-- except
+select stdName, region from stdTbl
+except
+select stdName, region from stdTbl;
