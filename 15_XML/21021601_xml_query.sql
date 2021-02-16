06a00c63271c340523b2ebf2473b12fa4816fb81
ghd0276 <ghd0276@naver.com>
2021-02-16 15:43:17 +0900

    2/15 XML 완료

---
+++ 15_XML/21021601_xml_query.sql
@@ -0,0 +1,6 @@
+﻿insert into xmlTbl values ('Hello World');
+
+select * from xmlTbl;
+
+insert into xmlTbl values ('<html><body><p>Sample HTML</p></body></html>');
+insert into xmlTbl values ('<html><body><p>Sample HTML</html></body>'); -- html과는 달리 형식에 엄격한 xml
---
+++ 15_XML/21021602_xml_json_query.sql
@@ -0,0 +1,7 @@
+﻿select u.userID, u.userName, u.birthYear, u.addr, u.height, b.prodName, b.price
+  from userTbl as u
+ inner join buyTbl as b
+    on u.userID = b.userID
+ where u.height > 170
+-- for xml auto;
+   for json auto;
