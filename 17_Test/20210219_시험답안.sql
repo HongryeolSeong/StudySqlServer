89a0211e9f3f21d50af63f0d43463139fa80a624
ghd0276 <ghd0276@naver.com>
2021-02-19 11:48:34 +0900

    2/19 SQL 마무리

--- 17_Test/20210219_시험답안.sql
+++ 17_Test/20210219_시험답안.sql
@@ -42,6 +42,27 @@ select b.Idx as 번호
     on b.Division = d.Division
  where b.Division = 'B002';
 
+-- 3-1 다르게 풀기(약식)
+select b.Idx 번호
+     , b.Division 장르번호 -- 장르 번호는 안됨 -> '장르 번호'
+	 , d.Names 장르
+	 , b.Names 책제목
+	 , b.Author 저자
+  from bookstbl b
+     , divtbl d
+ where b.Division = d.Division
+   and b.Division = 'B002';
+
+-- 3-1 다르게 풀기2(join없이 -> subquery 이용)
+select b.Idx as 번호
+     , b.Division as 장르번호
+	 --, d.Names as 장르
+	 , (select Names from divtbl where Division = b.Division) as 장르
+	 , b.Names as 책제목
+	 , b.Author as 저자
+  from bookstbl as b
+ where b.Division = 'B002';
+
 -- 3-2
 select m.Names
      , m.Levels
@@ -51,6 +72,14 @@ select m.Names
   left outer join rentaltbl as r
     on m.Idx = r.memberIdx
  where r.rentalDate is null;
+
+-- 3-2 다르게 풀기(subquery)
+select m.Names
+     , m.Levels
+	 , m.Addr
+	 , null as rentalDate
+  from membertbl as m
+ where m.Idx not in (select memberIdx from rentaltbl as r);
 
 -- 4-1
 insert into divtbl values ('I002', '자기계발서');
@@ -73,3 +102,17 @@ select d.Names
  inner join divtbl as d
     on b.Division = d.Division
  group by rollup(d.Names);
+
+-- 5 다르게 풀기
+select (select names from divtbl where Division = a.Division) as Names
+	    , a.sum_price as '총합계금액'
+  from
+	 (
+			select Division
+				 , sum(Price) as sum_price
+			  from bookstbl
+		  group by Division
+	 ) as a
+ union all
+select null, sum(price)
+  from bookstbl;
